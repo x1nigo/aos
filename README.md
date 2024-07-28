c8c3579d920ed11e53590cbf2d3dfe9f07ea9bc0
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-07-28 19:20:39 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# AOS (*alpine os*)
+
+- This is my post-install script for Alpine Linux.
+- (Still in progress...)
+
+# How to install
+
+1. `git clone https://github.com/x1nigo/aos.git`
+2. `cd aos`
+3. `chmod +x aos.sh && ./aos.sh`
---
+++ aos.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+dotfilesrepo="https://github.com/x1nigo/dotfiles.git"
+srcdir="/home/*/.local/src"
+mkdir -p /home/*/.local/src
+
+intro() {
+	echo "
+----------------------------
+W E L C O M E !
+----------------------------
+
+This is a minimal post-install script for Alpine Linux. Presumably,
+you already have a user account + password.
+
+Note: This should be run in the /root directory.
+
+Once ready, just hit <ENTER>. Otherwise, hit <CTRL-C> to quit.
+"
+read -r enter
+}
+
+set_privileges() {
+	echo "permit nopass :wheel" >> /etc/doas.d/doas.conf
+}
+
+set_xorg() {
+	setup-xorg-base
+}
+
+install_pkgs() {
+	sed progs.txt '/^#/d;/^$/d' > /tmp/progs.txt
+	while IFS=$'\n' read -r prog; do
+		apk add "$prog"
+	done < /tmp/progs.txt
+}
+
+compile_pkgs() {
+	for dir in $(echo "dwm st dmenu"); do
+		git -C "$srcdir" clone https://github.com/x1nigo/$dir.git
+		cd "$srcdir"/"$dir" && doas make clean install
+	done
+}
+
+get_dotfiles() {
+	git -C "$srcdir" clone "$dotfilesrepo"
+	cd "$srcdir"/dotfiles
+	shopt -s dotglob
+	cp -vr  * /home/*/
+
+	ln -s /home/*/.config/shell/shrc /home/*/.ashrc
+	ln -s /home/*/.config/nvim/init.vim /home/*/.vimrc
+}
+
+update_udev() {
+	mkdir -p /etc/X11/xorg.conf.d
+	echo "Section \"InputClass\"
+	Identifier \"touchpad\"
+	Driver \"libinput\"
+	MatchIsTouchpad \"on\"
+		Option \"Tapping\" \"on\"
+		Option \"NaturalScrolling\" \"on\"
+EndSection" > /etc/X11/xorg.conf.d/30-touchpad.conf
+}
+
+cleanup() {
+	cd
+	rm -r ~/aos
+	rm -r "$srcdir"/dotfiles
+	rm -r /home/*/.git
+	rm -r /home/*/README.md
+	mkdir -p /home/*/.local/run
+	find /home/*/.local/bin -type f -exec chmod +x {} \;
+}
+
+outro() {
+	echo "
+----------------------------
+D O N E !
+----------------------------
+
+Congratulations! You now have a working linux system on your device. Hit
+<ENTER> to reboot. User <CTRL-C> to cancel.
+"
+read -r enter
+reboot
+}
+
+main() {
+	intro
+	set_privileges
+	set_xorg
+	install_pkgs
+	compile_pkgs
+	get_dotfiles
+	update_udev
+	cleanup
+	outro
+}
+
+main
